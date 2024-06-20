94237c12fddc032662d1eb6046a686794ed58109
Nesesan <lounes.benz@outlook.fr>
2024-06-20 21:11:03 +0200

    add validators/add exceptions

--- src/main/java/com/nesesan/banking/dto/TransactionDto.java
+++ src/main/java/com/nesesan/banking/dto/TransactionDto.java
@@ -3,6 +3,9 @@ package com.nesesan.banking.dto;
 import com.nesesan.banking.models.Transaction;
 import com.nesesan.banking.models.TransactionType;
 import com.nesesan.banking.models.User;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -18,6 +21,9 @@ public class TransactionDto {
 
     private int id;
 
+    @Positive
+    @Max(value = 1000000)
+    @Min(value = 10)
     private BigDecimal amount;
 
     private TransactionType type;
--- src/main/java/com/nesesan/banking/dto/UserDto.java
+++ src/main/java/com/nesesan/banking/dto/UserDto.java
@@ -1,10 +1,13 @@
 package com.nesesan.banking.dto;
 
 import com.nesesan.banking.models.User;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -14,13 +17,30 @@ public class UserDto {
 
     private int id;
 
+    @NotNull(message = "firstname cannot be Null")
+    @NotEmpty(message = "firstname cannot be empty")
+    @NotBlank(message = "firstname cannot be blank")
     private String firstName;
 
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private String lastName;
 
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    @Email
     private String email;
 
+    @NotNull
+    @NotEmpty
+    @NotBlank
+    @Size(min = 8, max = 16)
     private String password;
+
+    @Past
+    private LocalDateTime birthdate;
 
     public static UserDto fromEntity(User user) {
         return UserDto.builder()
